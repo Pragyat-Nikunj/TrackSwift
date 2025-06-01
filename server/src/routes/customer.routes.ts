e43e679360f0fed0a282f5c1eadb81ef9e9c21c5
Pragyat Nikunj <99609060+Pragyat-Nikunj@users.noreply.github.com>
2025-06-02 01:13:30 +0530

    Update customer.routes.ts

--- server/src/routes/customer.routes.ts
+++ server/src/routes/customer.routes.ts
@@ -4,10 +4,11 @@ import {
   placeOrder,
   getOrderDetails,
   trackOrder,
+  getAllCustomerOrders,
 } from "../controllers/customer.controller";
 
 const router = express.Router();
-
+router.get("/orders", authMiddleware(["customer"]), getAllCustomerOrders);
 router.post("/orders", authMiddleware(["customer"]), placeOrder);
 router.get("/orders/:id", authMiddleware(["customer"]), getOrderDetails);
 router.get("/orders/:id/track", authMiddleware(["customer"]), trackOrder);
